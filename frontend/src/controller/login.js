4c1a3397124bc121857952b55f905a50d3206691
Rajat Shrivastava <rajat123468@gmail.com>
2021-06-20 23:07:58 +0530

    seperated frontend

--- frontend/src/controller/login.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const LoginUser = async (userData) => {
-    let response = await axios.post("https://gloiriobackend.herokuapp.com/login",userData);
-    console.log(response)
-    return response.data;
-}
-
-export default LoginUser;
